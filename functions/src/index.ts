8fe5f0a6ad764fa7963a183c2e43daf9b31a2a5c
tamemoto <tamemotoyou@gmail.com>
2021-01-28 16:54:50 +0900

    feat: create cloud function named publishers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import { collectionName } from './services/mangarel/constants';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+admin.initializeApp();
+
+export const publishers = functions
+  .region('asia-northeast2')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.publishers)
+      .get();
+    const data = snap.docs.map((doc) => doc.data());
+    res.send({ data });
+  });
--- functions/src/services/mangarel/models/publisher.ts
+++ functions/src/services/mangarel/models/publisher.ts
@@ -8,3 +8,11 @@ export type Publisher = {
   createdAt: firebase.firestore.Timestamp | null;
   updatedAt: firebase.firestore.Timestamp | null;
 };
+
+export const blankPublisher: Publisher = {
+  name: '',
+  nameReading: null,
+  website: null,
+  createdAt: null,
+  updatedAt: null,
+};
