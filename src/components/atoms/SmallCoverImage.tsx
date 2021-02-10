899a916744c118226640fcaf97d72765bbdcb08c
tamemoto <tamemotoyou@gmail.com>
2021-02-11 01:40:18 +0900

    feat: fix some file

---
+++ src/components/atoms/SmallCoverImage.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import Image, {
+  ImageProps,
+} from 'semantic-ui-react/dist/commonjs/elements/Image';
+
+const SmallCoverImage: FC<ImageProps> = ({ children, ...props }) => {
+  const CoverImage = styled(Image)`
+    filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.1));
+    margin-bottom: 0 !important;
+    width: 55px !important;
+  `;
+
+  return (
+    <CoverImage {...props} size="tiny" loading="lazy">
+      {children}
+    </CoverImage>
+  );
+};
+
+export default SmallCoverImage;
