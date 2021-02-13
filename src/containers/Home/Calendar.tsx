b67f2a6557be35abad8b24b566250c639ece98b7
tamemoto <tamemotoyou@gmail.com>
2021-02-13 20:41:44 +0900

    fix: update

--- src/containers/Home/Calendar.tsx
+++ src/containers/Home/Calendar.tsx
@@ -3,7 +3,7 @@ import useBooks from 'hooks/use-books';
 import Calendar from 'components/organisms/Home/Calendar';
 
 const CalendarContainer: FC = () => {
-  const { books, loading } = useBooks({ limit: 50 });
+  const { books, loading } = useBooks({ limit: 3 });
 
   return <Calendar books={books} loading={loading} />;
 };
